e7936d540703e5fb615a0d93f53c05cc583e2e4d
C-STYR <colestyron@gmail.com>
2021-03-22 21:11:54 -0800

    working TS version

---
+++ components/mainImage.tsx
@@ -0,0 +1,13 @@
+
+const mainImage: JSX.Element = 
+  <div className="imageContainer">
+      <img
+          className="bench"
+          src="/bench.jpg"
+          height="440"
+          width="300"
+          alt="the author examines plans at his workbench"
+      />
+  </div>
+
+  export default mainImage;

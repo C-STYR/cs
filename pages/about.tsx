0052b78887dcd1f56a0848560f554f3ad0de7e01
Cole Styron <colestyron@gmail.com>
2023-02-15 17:25:26 -0900

    updates /about

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import { link, p, section } from "../styles/about.css";
+import { link, p, pinkWords, section } from "../styles/about.css";
 import Link from "next/link";
 
 export default function About() {
@@ -6,16 +6,41 @@ export default function About() {
     <section className={section}>
       <h1>Hello</h1>
       <div className={p}>
-        <p>I'm Cole.</p>
         <p>
-          I am a fullstack software engineer, but I mainly write frontend code.
+          I ocassionally post to this{" "}
+          <Link href="/blog" className={link}>
+            web log
+          </Link>
+          , usually on topics that don't have a great presence on the interwebs
+          already, and almost exclusively on software engineering.
+        </p>
+        <p>
+          If you're interested in{" "}
+          <span className={pinkWords}>wooden boat building</span>, you can find
+          some pictures{" "}
+          <Link
+            className={link}
+            href="https://flickr.com/photos/colestyron/albums/"
+          >
+            here
+          </Link>
+          .
+        </p>
+        <p>
+          This site was built with <span className={pinkWords}>Next.js</span>{" "}
+          and <span className={pinkWords}>vanilla-extract CSS</span>. The source
+          code can be found{" "}
+          <Link className={link} href="https://github.com/c-styr/cs">
+            here
+          </Link>
+          .
         </p>
         <p>
           If you'd like to hire me, you can find a copy of my resume{" "}
           <Link className={link} href="">
             here
           </Link>
-          .
+          . It's <i>probably</i> not the most recent version.
         </p>
       </div>
     </section>
--- styles/about.css.ts
+++ styles/about.css.ts
@@ -4,13 +4,24 @@ import theme from "./theme";
 export const section = style({
   marginLeft: 70,
   marginTop: 60,
+  maxWidth: 900,
+  '@media': {
+    'screen and (min-width: 768px)': {
+        marginLeft: 20,
+    }
+  }
 });
 
 export const p = style({
   fontSize: 20,
+//   marginBottom: 160,
 });
 
 export const link = style({
   textDecoration: "none",
   color: theme.aqua,
+});
+
+export const pinkWords = style({
+  color: theme.pink,
 });
